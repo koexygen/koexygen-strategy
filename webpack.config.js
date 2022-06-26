49db599f43a72a923916bd2727d9480599308fa2
Koexygen <rexarkoexygen@gmail.com>
2022-06-26 13:27:35 +0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -145,7 +145,7 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-128.png',
+          from: 'src/assets/img/icon.png',
           to: path.join(__dirname, 'build'),
           force: true,
         },
